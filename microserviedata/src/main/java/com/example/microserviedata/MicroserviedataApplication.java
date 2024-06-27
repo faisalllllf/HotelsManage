8982db49f8fbe05e9b11f57de84824982c665fb5
faisal <shaikfaisalbawaer000@gmail.com>
2024-06-27 15:02:51 +0530

    tresting

---
+++ microserviedata/src/main/java/com/example/microserviedata/MicroserviedataApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviedataApplication.class, args);
+	}
+
+}
